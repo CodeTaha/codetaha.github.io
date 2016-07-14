9db2c29e421234b65e658941f2f2f8989162c3fb
codetaha <taha.kachwala21@gmail.com>
2016-07-14 18:56:20 +0300

    Resume link updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,8 +13,10 @@ $(document).ready(function() {
             onreveal: function(e){
                 console.log(e);
             }
-        });
-
+      });
+      var resume_link='https://drive.google.com/file/d/0ByFlywQkLFB5M3dZcUxzSVE5Nm8/view?usp=sharing';
+      $(".icons").prepend('<li><a href="'+resume_link+'" title="resume" target="_blank" class="icon fa fa-file-text"><span class="label">Download CV</span></a></li>');
+      $("#resume_link").attr("href",resume_link);
       portfolio_json = {
       	"ConstellationAtlas":{
       		"title": "Constellation Atlas",
